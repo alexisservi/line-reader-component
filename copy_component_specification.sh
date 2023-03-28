f6f256d373ace4c3703427590592f6302df318b3
alexis <alexis.ortega@servinformacion.com>
2023-03-27 20:29:50 -0500

    change input specification

--- copy_component_specification.sh
+++ copy_component_specification.sh
@@ -1,1 +1,2 @@
 gsutil cp kubeflow_component_spec.yaml gs://ml-auto-pipelines-bucket/components-yamls/line-reader-writer/kubeflow_component_spec.yaml
+gsutil acl ch -u AllUsers:R gs://ml-auto-pipelines-bucket/components-yamls/line-reader-writer/kubeflow_component_spec.yaml
