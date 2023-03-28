87062e3a8f7ae6f6e29f883f8b4ed0b1a0d074b9
alexis <alexis.ortega@servinformacion.com>
2023-03-27 20:41:06 -0500

    uding cloudsdk builder

--- copy_component_specification.sh
+++ copy_component_specification.sh
@@ -1,2 +1,5 @@
+curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-423.0.0-linux-x86_64.tar.gz
+tar -xf google-cloud-cli-423.0.0-linux-x86_64.tar.gz
+./google-cloud-sdk/install.sh
 gsutil cp kubeflow_component_spec.yaml gs://ml-auto-pipelines-bucket/components-yamls/line-reader-writer/kubeflow_component_spec.yaml
 gsutil acl ch -u AllUsers:R gs://ml-auto-pipelines-bucket/components-yamls/line-reader-writer/kubeflow_component_spec.yaml
